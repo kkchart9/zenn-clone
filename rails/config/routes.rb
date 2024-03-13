96cfe88bce679a0b3070326255df54c68e4c4fc2
kkchart9 <koudai200109@gmail.com>
2024-03-14 01:31:13 +0900

    ログイン機能を作成&テスト

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,6 +9,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
+      mount_devise_token_auth_for "User", at: "auth"
+
+      namespace :current do
+        resource :user, only: [:show]
+      end
     end
   end
 end
